c0eb21c8d0dad81b4959a6c342dc8c82f5b77328
Wilson Franca <wilson.franca.92@gmail.com>
2023-11-12 07:54:08 -0800

    refactor/tests(use-cases/register): created in-memory repository to unit test register new users use case

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,5 +1,59 @@
-import { expect, test } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-reposotory'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { RegisterUseCase } from './register'
 
-test('check if vitest config works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should be able to register a user', async () => {
+    const usersReposotory = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersReposotory)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Jane Doe',
+      email: 'janedoe@eample.com',
+      password: '12345678',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersReposotory = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersReposotory)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Jane Doe',
+      email: 'janedoe@eample.com',
+      password: '12345678',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '12345678',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersReposotory = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersReposotory)
+
+    const email = 'janedoe@eample.com'
+
+    await registerUseCase.execute({
+      name: 'Jane Doe',
+      email,
+      password: '12345678',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Jane Doe',
+        email,
+        password: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
 })
