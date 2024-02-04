8ca0a8b3b45bf806328683db66ee8a3458177c00
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-04 06:58:56 -0800

    feat(use-cases/get-user-profile): implement findById method in InMemoryUsersRepository, updating typo in file name

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,4 +1,4 @@
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-reposotory'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
