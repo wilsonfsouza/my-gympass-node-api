761023921cf5b334b14a2945022ac81809823383
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-04 07:01:05 -0800

    feat(use-cases/get-user-profile): add get user profile use case with its unit tests

---
+++ src/http/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password_hash: await hash('12345678', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('Jane Doe')
+  })
+
+  it('should be not be able to get user profile with wrong id', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
