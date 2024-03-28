7564ee7e21a21959a6c17bc4717940432ad3acc8
Wilson Franca <wilson.franca.92@gmail.com>
2024-03-28 06:11:08 -0700

    feat(http): create auth flow

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated Routes
+  app.get('/me', profile)
 }
