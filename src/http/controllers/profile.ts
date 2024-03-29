1a72de9dc87ab061d452066f87d22efc2c43832c
Wilson Franca <wilson.franca.92@gmail.com>
2024-03-28 18:18:03 -0700

    feat(http/controllers): implement profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileUseCase } from '../use-cases/factories/make-get-user-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
