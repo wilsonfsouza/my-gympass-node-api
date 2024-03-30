aa78b9157ebbc495db8c8ee526634a2e0baba2e5
Wilson Franca <wilson.franca.92@gmail.com>
2024-03-30 07:19:20 -0700

    fix(http/controllers): correct imports

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { UserAlreadyExistsError } from '../use-cases/errors/user-already-exists-error'
-import { makeRegisterUseCase } from '../use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
