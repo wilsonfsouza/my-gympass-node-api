70abd9132fb64883ddce3b1f769c5a14641098dd
Wilson Franca <wilson.franca.92@gmail.com>
2024-04-01 06:08:12 -0700

    tests(http/controller): add e2e test to authenticate controller

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,31 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Authenticate Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'janedoe@example.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
